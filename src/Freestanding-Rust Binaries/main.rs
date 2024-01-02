85e4ddf1ea49ae29b64acb37d5ee35751a31efe2
Affan Pathan <pathan.affan02gmail.com>
2024-01-02 19:24:12 +0530

    added freestanding binaries for rust

---
+++ src/Freestanding-Rust Binaries/main.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/*
+cargo.toml:
+
+[package]
+name = "crate_name"
+version = "0.1.0"
+authors = ["Author Name <author@example.com>"]
+
+# the profile used for `cargo build`
+[profile.dev]
+panic = "abort" # disable stack unwinding on panic
+
+# the profile used for `cargo build --release`
+[profile.release]
+panic = "abort" # disable stack unwinding on panic
+
+*/
+
+/*
+TO run the freestanding binary
+
+rustup target add thumbv7em-none-eabihf
+cargo build --target thumbv7em-none-eabihf
+
+*/
