a45a163100e758b6439dc319d7d006cf6f81cd58
Affan Pathan <pathan.affan02gmail.com>
2024-01-03 19:24:13 +0530

    day 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut array:[i32;5]=[1,2,3,4,5];
+    array.reverse();
+    println!("{:?}", array);
+
+    let arr2=[1,2,3,4,5,6,7,8];
+    let rev_iter=arr2.iter().rev();
+    let rev_array:Vec<_>=rev_iter.collect();
+    println!("{:?}",rev_array)
 }
